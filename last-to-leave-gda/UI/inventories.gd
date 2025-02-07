5c74ec28a7182b45521365ffb7bbd880e75657e4
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-02-07 15:43:54 -0800

    Testing the pop-up inventory input
    Only added some code to test the pop-up. The inventory is not yet functional, and doesn't properly load the item slots into the menu.
    
    This issue still needs to be fixed, and it's been very difficult trying to fix it.

--- last-to-leave-gda/UI/inventories.gd
+++ last-to-leave-gda/UI/inventories.gd
@@ -1,11 +1,11 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var inventories: Control = $/root/World/Inventories
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("open_inventory") and inventories.visible == false:
+		inventories.visible = true
+	elif Input.is_action_just_pressed("open_inventory") and inventories.visible == true:
+		inventories.visible = false
