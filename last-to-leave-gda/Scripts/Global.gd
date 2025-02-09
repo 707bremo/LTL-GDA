9f08a74da110c0db4f84b074dc3d0382d967fa60
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-02-08 21:18:10 -0800

    Making the inventory item and testing it

--- last-to-leave-gda/Scripts/Global.gd
+++ last-to-leave-gda/Scripts/Global.gd
@@ -13,8 +13,17 @@ func _ready():
 	inventory.resize(30)
 
 
-func add_inventory_item():
-	inventory_updated.emit()
+func add_inventory_item(item):
+		for i in range(inventory.size()):
+			if inventory[i] != null and inventory[i]["item_type"] == item["item_type"] and inventory[i]["item_description"] == item["item_description"]:
+				inventory[i]["quantity"] == item["quantity"]
+				inventory_updated.emit()
+				return true
+			elif inventory[i] == null:
+				inventory[i] = item
+				inventory_updated.emit()
+				return true
+			return false
 	
 func remove_inventory_item():
 	inventory_updated.emit()
--- last-to-leave-gda/Scripts/inventory_item.gd
+++ last-to-leave-gda/Scripts/inventory_item.gd
@@ -1,10 +1,51 @@
+@tool
 extends Node3D
+
+
 
 @export var item_name = ""
 @export var item_type = ""
 @export var item_description = ""
 
-var scene_path: String = ""
+
+@onready var pickup_label: Label3D = $Pickup_label
+var scene_path: String = "res://Scenes/inventory_item.tscn"
+var player_in_range = false
+
+
+
 
 func _ready() -> void:
 	pass 
+
+
+# If the player presses the "E" key, and is within range of the item, the player can pick it up
+func _process(delta: float) -> void:
+	if player_in_range and Input.is_action_just_pressed("ui_add"):
+		pickup_item()
+
+
+
+func pickup_item():
+	var item = {
+		"quantity" : 1,
+		"item_name" : item_name,
+		"item_type" : item_type,
+		"item_description" : item_description,
+		"scene_path" : scene_path,
+	}
+	if Global.player_node:
+		Global.add_inventory_item(item)
+		self.queue_free()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		player_in_range = true
+		pickup_label.visible = true
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		player_in_range = false
+		pickup_label.visible = false
