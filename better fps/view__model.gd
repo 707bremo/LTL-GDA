9ca664b7327428f2f85a5b316e136079e5d62d8f
Josh <jovazquez1@s.sfusd.edu>
2025-02-13 22:04:52 -0800

    completed mechanics

--- better fps/view__model.gd
+++ better fps/view__model.gd
@@ -1,5 +1,7 @@
 extends Camera3D
 @onready var fps_rig: Node3D = $fps_rig
+@onready var animation_player: AnimationPlayer = $fps_rig/shotgun/AnimationPlayer
+var shotgun_in_use = true; 
 
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,3 +13,18 @@ func _process(delta: float) -> void:
 func sway(sway_amount):
 	fps_rig.position.x -= sway_amount.x * 0.00005
 	fps_rig.position.y += sway_amount.y * 0.00005
+
+func _input(event):
+	if(event.is_action_pressed("shoot")):
+		animation_player.play("fire")
+	
+	if(event.is_action_pressed("reload")):
+		animation_player.play("reload")
+	
+	if(event.is_action_pressed("use")):
+		shotgun_in_use = !shotgun_in_use
+		if(shotgun_in_use):
+			animation_player.play("put_away")
+		else:
+			animation_player.play("pull_up")
+	
