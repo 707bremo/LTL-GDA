72b9f7b8f1a63f1bd05ff8c0855ce0cb6b307f62
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-03-01 19:02:24 -0800

    First inventory test success
    We're checking for a couple of factors:
    
    - The slots appear on the screen
    - Image loads
    
    The next thing to make sure is that the correct item is displayed, as well as some other adjustments to the UI. We'll also make it so that the inventory anchors to the right spot when loading.

--- last-to-leave-gda/UI/Inventory/inventory.gd
+++ last-to-leave-gda/UI/Inventory/inventory.gd
@@ -4,6 +4,10 @@ const Slot = preload("res://UI/Inventory/slot.tscn")
 
 @onready var item_grid: GridContainer = $MarginContainer/ItemGrid
 
+func _ready() -> void:
+	var inv_data = preload("res://test_inv.tres")
+	populate_item_grid(inv_data.slot_datas)
+
 
 func populate_item_grid(slot_datas: Array[SlotData]) -> void:
 	for child in item_grid.get_children():
@@ -12,6 +16,6 @@ func populate_item_grid(slot_datas: Array[SlotData]) -> void:
 	for slot_data in slot_datas:
 		var slot = Slot.instantiate()
 		item_grid.add_child(slot)
-	
+		
 		if slot_data:
 			slot.set_slot_data(slot_data)
