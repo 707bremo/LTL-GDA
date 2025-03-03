16564acba7bdb25763d81d47696bfd1bca7e779d
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-03-02 19:44:53 -0800

    Drop functionality added
    The items can now drop out of the inventory in single or stacked instances. Left-clicking drops the entire stack, whereas right-clicking drops one item.
    
    It works okay, but needs fine tuning and bug fixing.
    
    💎

--- last-to-leave-gda/Main.gd
+++ last-to-leave-gda/Main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const Pickup = preload("res://UI/Inventory/item/pickup.tscn")
+
 @onready var test_player: CharacterBody3D = $test_player
 @onready var inv_interface: Control = $UI/InvInterface
 
@@ -23,3 +25,10 @@ func toogle_inv_interface(external_inventory_owner = null) -> void:
 		inv_interface.set_external_inventory(external_inventory_owner)
 	else:
 		inv_interface.clear_external_inventory()
+
+
+func _on_inv_interface_drop_slot_data(slot_data: SlotData) -> void:
+	var pick_up = Pickup.instantiate()
+	pick_up.slot_data = slot_data
+	pick_up.position = test_player.get_drop_position()
+	add_child(pick_up)
