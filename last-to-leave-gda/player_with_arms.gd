ec9677283894563016bcea62a6b3ef50ad16a368
Josh <jovazquez1@s.sfusd.edu>
2025-03-06 21:10:01 -0800

    bullets (partially functional)

--- last-to-leave-gda/player_with_arms.gd
+++ last-to-leave-gda/player_with_arms.gd
@@ -20,19 +20,15 @@ func _input(event):
 
 func _physics_process(delta):
 	$"head/Camera3D/SubViewportContainer/SubViewport/view_ model_camera".global_transform = camera.global_transform
-	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
--- last-to-leave-gda/view__model.gd
+++ last-to-leave-gda/view__model.gd
@@ -1,7 +1,10 @@
 extends Camera3D
 @onready var fps_rig: Node3D = $fps_rig
 @onready var animation_player: AnimationPlayer = $fps_rig/shotgun/AnimationPlayer
+@onready var gun_barrel = $fps_rig/shotgun/shotgun_model/RayCast3D
 var shotgun_in_use = true; 
+var bullet = load("res://bullet.tscn")
+var instance
 
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,6 +13,7 @@ func _process(delta: float) -> void:
 	fps_rig.position.x = lerp(fps_rig.position.x, 0.0, delta * 5)
 	fps_rig.position.y = lerp(fps_rig.position.y, 0.0, delta * 5)
 
+
 func sway(sway_amount):
 	fps_rig.position.x -= sway_amount.x * 0.00005
 	fps_rig.position.y += sway_amount.y * 0.00005
@@ -17,14 +21,17 @@ func sway(sway_amount):
 func _input(event):
 	if(event.is_action_pressed("shoot")):
 		animation_player.play("fire")
-	
+		instance = bullet.instantiate()
+		instance.position = gun_barrel.global_position
+		instance.transform.basis = gun_barrel.global_transform.basis
+		get_parent().add_child(instance)
+
 	if(event.is_action_pressed("reload")):
 		animation_player.play("reload")
-	
+
 	if(event.is_action_pressed("use")):
 		shotgun_in_use = !shotgun_in_use
 		if(shotgun_in_use):
 			animation_player.play("put_away")
 		else:
 			animation_player.play("pull_up")
-	
