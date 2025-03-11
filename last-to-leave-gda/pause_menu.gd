0a72b4e20542ade0f04ad0505e3599b3898271a1
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-03-10 20:38:19 -0700

    Start Menu fully implemented
    We'll most likely change the way it looks later, but at least it functions the way it's intended to.
    
    The load button however, will have it's functionality later when we make save points.

--- last-to-leave-gda/pause_menu.gd
+++ last-to-leave-gda/pause_menu.gd
@@ -1,9 +1,16 @@
 extends Control
 
 
+
+func _ready():
+	$blurAnim.play("RESET")
+	hide()
+
+
 func resume_game():
 	get_tree().paused = false
 	$blurAnim.play_backwards("blurmenu")
+	hide()
 
 func load_game():
 	pass
@@ -11,14 +18,18 @@ func load_game():
 func pause_game():
 	get_tree().paused = true
 	$blurAnim.play("blurmenu")
+	show()
 
 func test_pause_menu():
 	if Input.is_action_just_pressed("pause_game") and get_tree().paused == false:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		pause_game()
 	elif Input.is_action_just_pressed("pause_game") and get_tree().paused == true:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		resume_game()
 
 func _on_continue_pressed() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	resume_game()
 
 func _on_load_pressed() -> void:
@@ -26,3 +37,7 @@ func _on_load_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _process(delta: float) -> void:
+	test_pause_menu()
