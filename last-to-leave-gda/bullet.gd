8b8490d8e9c6f1483beea5268d78cf750699d021
Josh <jovazquez1@s.sfusd.edu>
2025-03-12 21:23:57 -0700

    imported bullet + fixes

--- last-to-leave-gda/bullet.gd
+++ last-to-leave-gda/bullet.gd
@@ -1,16 +1,8 @@
 extends Node3D
-const SPEED = 30.0
-const GRAVITY = -9.8  
-@onready var mesh = $MeshInstance3D
-@onready var ray = $RayCast3D
-@onready var particles = $GPUParticles3D
+const SPEED = 50.0
+const GRAVITY = -2.8  
 var velocity = Vector3(0, 0, -SPEED) 
 
 func _process(delta: float) -> void:
 	velocity.y += GRAVITY * delta  
 	position += transform.basis * velocity * delta  
-	if ray.is_colliding():
-		mesh.visible = false
-		particles.emitting = true
-		await get_tree().create_timer(1.0).timeout
-		queue_free()
