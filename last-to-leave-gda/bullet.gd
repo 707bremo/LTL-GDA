4ee76d621543e69efd570c07ee22cacf2fd3f70e
Josh <jovazquez1@s.sfusd.edu>
2025-03-12 19:00:32 -0700

    Gun mechanics fixed
    fixed shooting timer
    fixed missing sfx
    fixed shooting anim errors

--- last-to-leave-gda/bullet.gd
+++ last-to-leave-gda/bullet.gd
@@ -1,19 +1,16 @@
 extends Node3D
 const SPEED = 30.0
+const GRAVITY = -9.8  
 @onready var mesh = $MeshInstance3D
-@onready var ray =  $RayCast3D
+@onready var ray = $RayCast3D
 @onready var particles = $GPUParticles3D
-
-func _ready() -> void:
-	pass
+var velocity = Vector3(0, 0, -SPEED) 
 
 func _process(delta: float) -> void:
-	position += transform.basis * Vector3(0, 0, -SPEED) * delta
+	velocity.y += GRAVITY * delta  
+	position += transform.basis * velocity * delta  
 	if ray.is_colliding():
 		mesh.visible = false
 		particles.emitting = true
 		await get_tree().create_timer(1.0).timeout
 		queue_free()
-
-func _on_timer_timeout() -> void:
-	queue_free()
--- last-to-leave-gda/view__model.gd
+++ last-to-leave-gda/view__model.gd
@@ -2,36 +2,46 @@ extends Camera3D
 @onready var fps_rig: Node3D = $fps_rig
 @onready var animation_player: AnimationPlayer = $fps_rig/shotgun/AnimationPlayer
 @onready var gun_barrel = $fps_rig/shotgun/shotgun_model/RayCast3D
-var shotgun_in_use = true; 
+@onready var shoot_timer: Timer = $Shoot_timer  
+@onready var shoot_sound: AudioStreamPlayer3D = $shoot_sound
+@onready var shotgun_cock_back_sound: AudioStreamPlayer3D = $shotgun_cock_back_sound
+@onready var holster_sound: AudioStreamPlayer3D = $holster_sound
+var shotgun_in_use = true
 var bullet = load("res://bullet.tscn")
 var instance
+var can_shoot = true  
 
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 func _process(delta: float) -> void:
 	fps_rig.position.x = lerp(fps_rig.position.x, 0.0, delta * 5)
 	fps_rig.position.y = lerp(fps_rig.position.y, 0.0, delta * 5)
 
-
-func sway(sway_amount):
+func sway(sway_amount) -> void:
 	fps_rig.position.x -= sway_amount.x * 0.00005
 	fps_rig.position.y += sway_amount.y * 0.00005
 
-func _input(event):
-	if(event.is_action_pressed("shoot")):
+func _input(event) -> void:
+	if event.is_action_pressed("shoot") and can_shoot:
+		can_shoot = false  
 		animation_player.play("fire")
+		shoot_sound.play()
 		instance = bullet.instantiate()
 		instance.position = gun_barrel.global_position
 		instance.transform.basis = gun_barrel.global_transform.basis
 		get_parent().add_child(instance)
-
-	if(event.is_action_pressed("reload")):
-		animation_player.play("reload")
-
-	if(event.is_action_pressed("use")):
+		await animation_player.animation_finished
+		animation_player.play("shotgun_cock_back")
+		shotgun_cock_back_sound.play()
+		await animation_player.animation_finished
+		can_shoot = true
+	elif event.is_action_pressed("use"):
+		holster_sound.play()
 		shotgun_in_use = !shotgun_in_use
-		if(shotgun_in_use):
+		if shotgun_in_use:
 			animation_player.play("put_away")
+			can_shoot = false
 		else:
 			animation_player.play("pull_up")
+			can_shoot = true
