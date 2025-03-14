5c6e0ecc56a03bb5fb94b96c73144b2c20592606
blue9200 <Josantoyo@s.sfusd.edu>
2025-03-13 22:57:55 -0700

    Mouse Updated Pt.1

--- last-to-leave-gda/player.gd
+++ last-to-leave-gda/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody3D
  
-@export var inventory_data: InventoryData
+@export var SPEED : float = 5.0
+@export var JUMP_VELOCITY : float = 4.5
+@export var MOUSE_SENSITIVITY : float = 0.5
+@export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
+@export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
+@export var CAMERA_CONTROLLER : Camera3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
- 
 var _mouse_input : bool = false
+var _mouse_rotation : Vector3
 var _rotation_input : float
 var _tilt_input : float
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -13,16 +16,27 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
  
 func _input(event):
 	if event.is_action_pressed("exit"):
-		get_tree().q
+		get_tree().quit()
 
-func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
- 
- 
 func _unhandled_input(event: InputEvent) -> void:
-	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode()
-Input.MOUSE_MODE_CAPTURED
-
+	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	if _mouse_input:
+		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
+		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
+		print(Vector2(_rotation_input,_tilt_input))
+		
+func _update_camera(delta): 
+	_mouse_rotation.x += _tilt_input * delta
+	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	_mouse_rotation.y += _rotation_input * delta
+	
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_mouse_rotation)
+	CAMERA_CONTROLLER.rotation.z = 0.0 
+	_rotation_input = 0.0
+	_tilt_input = 0.0
+	
+func  _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
  
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
@@ -34,6 +48,8 @@ func _physics_process(delta: float) -> void:
  
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	_update_camera(delta)
  
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
