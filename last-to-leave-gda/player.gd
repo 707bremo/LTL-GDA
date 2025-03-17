fd0daaf7b678a36f60406824ddca1228d78e020b
blue9200 <Josantoyo@s.sfusd.edu>
2025-03-16 20:49:10 -0700

    updated character movement

--- last-to-leave-gda/player.gd
+++ last-to-leave-gda/player.gd
@@ -1,63 +1,68 @@
 extends CharacterBody3D
- 
-@export var SPEED : float = 5.0
-@export var JUMP_VELOCITY : float = 4.5
-@export var MOUSE_SENSITIVITY : float = 0.5
-@export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
-@export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
-@export var CAMERA_CONTROLLER : Camera3D
 
-var _mouse_input : bool = false
-var _mouse_rotation : Vector3
-var _rotation_input : float
-var _tilt_input : float
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
- 
-func _input(event):
-	if event.is_action_pressed("exit"):
-		get_tree().quit()
+var speed
+const FATIGUED = 3.22
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
+const JUMP_VELOCITY = 4.8
+const SENSITIVITY = 0.004
 
-func _unhandled_input(event: InputEvent) -> void:
-	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
-	if _mouse_input:
-		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
-		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
-		print(Vector2(_rotation_input,_tilt_input))
-		
-func _update_camera(delta): 
-	_mouse_rotation.x += _tilt_input * delta
-	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
-	_mouse_rotation.y += _rotation_input * delta
-	
-	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_mouse_rotation)
-	CAMERA_CONTROLLER.rotation.z = 0.0 
-	_rotation_input = 0.0
-	_tilt_input = 0.0
-	
-func  _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
- 
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().quit()
- 
- 
-func _physics_process(delta: float) -> void:
+#bob variables
+const BOB_FREQ = 2.4
+const BOB_AMP = 0.08
+var t_bob = 0.0
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
+var gravity = 9.8
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-100), deg_to_rad(60))
+
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	return pos
+
+func _process(delta: float) -> void:
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (head.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+	move_and_slide()
+
+func _physics_process(delta):
+	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
- 
+
+	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	_update_camera(delta)
- 
-	var input_dir := Input.get_vector("left", "right", "forward", "back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
- 
-	move_and_slide()
+		speed = WALK_SPEED
