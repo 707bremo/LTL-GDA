007060a573bfcb854353234e87d5c4acefef30f6
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-04-28 11:53:20 -0700

    Readability for footstep Script
    Added comments to help understand what's happening around the script.

--- last-to-leave-gda/player/footstep_manager.gd
+++ last-to-leave-gda/player/footstep_manager.gd
@@ -10,9 +10,20 @@ func _ready() -> void:
 	player.step.connect(play_sound)
 	
 func play_sound():
+	
+	
+	# create a new 3D audio player node for spatialized sound
 	var audio_player : AudioStreamPlayer3D = AudioStreamPlayer3D.new()
+	
+	
 	var random_index : int = randi_range(0, footstep_sounds.size() - 1)
+	
+	# select a random sound from the exported array
 	audio_player.stream = footstep_sounds[random_index]
+	
+	# alter footstep pitch for natural variation
+	# added as a child of position node so it plays in that area
+	# finished signal and destory func. are there to conserve memory
 	audio_player.pitch_scale = randf_range(0.8, 1.2)
 	ground_pos.add_child(audio_player)
 	audio_player.play()
