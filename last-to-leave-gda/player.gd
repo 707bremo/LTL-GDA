34ba6c4f5ac9746492bec1667039596ed08e0832
Josh <jovazquez1@s.sfusd.edu>
2025-05-04 23:08:20 -0700

    quick fixes for doors + played camera debug

--- last-to-leave-gda/player.gd
+++ last-to-leave-gda/player.gd
@@ -2,16 +2,16 @@ extends CharacterBody3D
 
 var speed
 const WALK_SPEED = 5.0
-const SPRINT_SPEED = 8.0
+const SPRINT_SPEED = 50.0
 const JUMP_VELOCITY = 4.8
 const SENSITIVITY = 0.004
 
-#bob variables
+# bob variables
 const BOB_FREQ = 2.4
 const BOB_AMP = 0.08
 var t_bob = 0.0
 
-#fov variables
+# fov variables
 const BASE_FOV = 75.0
 const FOV_CHANGE = 1.5
 
@@ -19,34 +19,33 @@ var gravity = 13
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
-
+@onready var raycast = $Head/RayCast3D
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
-
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
+	# Handle Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	if Input.is_action_pressed("sprint"):
-		speed = SPRINT_SPEED
-	else:
-		speed = WALK_SPEED
+	# Sprinting
+	speed = SPRINT_SPEED if Input.is_action_pressed("sprint") else WALK_SPEED
 
-	# Get the input direction and handle the movement/deceleration.
+	# Movement
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	if is_on_floor():
 		if direction:
 			velocity.x = direction.x * speed
@@ -67,6 +66,13 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+	# Door interaction
+	if Input.is_action_just_pressed("interact"):
+		if raycast.is_colliding():
+			var collider = raycast.get_collider()
+			if collider.is_in_group("door") and "interact" in collider:
+				collider.interact()
+				print("raycast collide with door")
 
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
