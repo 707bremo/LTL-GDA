513a99c624d48a5b86068670a17bdc294a4574e3
blue9200 <Josantoyo@s.sfusd.edu>
2025-05-08 22:19:06 -0700

    4 weapon types added

--- last-to-leave-gda/weapon_manager/weapon_manager.gd
+++ last-to-leave-gda/weapon_manager/weapon_manager.gd
@@ -5,7 +5,6 @@ extends Node3D
 
 @export var player : CharacterBody3D
 @export var bullet_raycast : RayCast3D
-
 @export var view_model_container : Node3D
 @export var world_model_container : Node3D
 
