81c5df3e72e68dd22d6886b6c63856e7e9e584b3
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-05-11 21:30:31 -0700

    Added sound bus and tuned animation

--- last-to-leave-gda/main_menu.gd
+++ last-to-leave-gda/main_menu.gd
@@ -1,6 +1,13 @@
 extends Control
 
+@onready var menu_player = $MenuPlayer
+@onready var menu_music: AudioStreamPlayer = $SoundsNmoozic/MenuMusic
+
 
 func _ready() -> void:
-	$MenuMusic.play()
-	$AnimationPlayer.play("main_opening")
+	menu_music.play()
+	menu_player.play("main_opening")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		$SoundsNmoozic/main_select.play()
