81848243e0ba685855eb6ac831cdf4732ba9ae07
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-05-15 00:48:58 -0700

    Making a loading screen, added some animations
    Some looping elements were added as well as the loading screen itself. We were able to make it so that the loading text was animated as it was loading.
    
    The loading screen can also be accessed from anywhere meaning that a new loading screen wouldn't have to be made every single time a new scene would load. It would just instantiate itself in the needed time.

---
+++ last-to-leave-gda/loading_screen.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+@export_file("*. tscn") var next_scene_path: String  # Determines which scene to load
+@onready var loading_label: AnimationPlayer = $loading_label
+@onready var loading_rods: AnimationPlayer = $elements/loading_rods
+
+
+func _ready():
+	ResourceLoader.load_threaded_request(next_scene_path)
+	loading_label.play("loading_text")
+	loading_rods.play("load_opening")
+	loading_rods.play("looped_elements")
+
+func _process(delta):
+	if ResourceLoader.load_threaded_get_status(next_scene_path) == ResourceLoader.THREAD_LOAD_LOADED:
+		set_process(false)
+		await get_tree().create_timer(15).timeout
+		var new_scene: PackedScene = ResourceLoader.load_threaded_get(next_scene_path)
+		get_tree().change_scene_to_packed(new_scene)
+	
--- last-to-leave-gda/main_menu.gd
+++ last-to-leave-gda/main_menu.gd
@@ -1,13 +1,70 @@
 extends Control
 
+
+# ALL UI ELEMENTS
 @onready var menu_player = $MenuPlayer
 @onready var menu_music: AudioStreamPlayer = $SoundsNmoozic/MenuMusic
+@onready var play_button : Button = $play
+@onready var transfer_screen_blocker: Control = $transfer_screen
+@onready var bg: ColorRect = $bg
+@onready var siimlogo: Sprite2D = $siimlogo
+@onready var title: Label = $title
+@onready var lobby_screen: Control = $lobby_screen
+@onready var play_game_button: Button = $lobby_screen/play_game
+@onready var selection_bg: ColorRect = $lobby_screen/selection_bg
+
+
+# conditions ONLY for screen verification
+var is_start_screen : bool = true
+var is_lobby_screen : bool = true
+
 
 
 func _ready() -> void:
+	Input.MOUSE_MODE_VISIBLE
+	is_start_screen = true
+	is_lobby_screen = false
 	menu_music.play()
 	menu_player.play("main_opening")
 
+
+
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+	
+	
+# press the enter button to transfer from the start, to the lobby
+	if Input.is_action_just_pressed("ui_accept") and is_start_screen:
+		is_start_screen = false
+		play_button.emit_signal("pressed")
 		$SoundsNmoozic/main_select.play()
+	
+
+
+
+func transfer_screen():
+	menu_player.play("transfer_to_lobby")
+
+func stop_transfer(anim_name: StringName) -> void:
+	if anim_name == "transfer_to_lobby":
+		lobby_screen.visible = true
+		is_lobby_screen = true
+		play_button.disabled = true
+		title.visible = false
+		siimlogo.visible = false
+		play_button.visible = false
+		menu_player.play("lobby_opening")
+
+
+	if anim_name == "selected":
+		selection_bg.visible = false
+		transfer_to_game()
+
+
+
+func play_game_pressed():
+	selection_bg.visible = true
+	menu_player.play("selected")
+
+
+func transfer_to_game():
+	LoadingScreenFunctions.load_screen_to_scene("res://world/test_room.tscn")
