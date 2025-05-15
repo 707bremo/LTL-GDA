0e15280efc8f7e6cbcc5eb4a824b2e8ec2208fab
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-05-15 10:23:55 -0700

    Fixing a small issue w/ the animation player
    The loading screen had additional colorRects that were supposed to appear on the screen but didn't play their animation.
    
    This fix was easy, simply by making sure that it plays before the elements that loop above and below the screen. Everything else works fine for now.

--- last-to-leave-gda/loading_screen.gd
+++ last-to-leave-gda/loading_screen.gd
@@ -9,7 +9,6 @@ func _ready():
 	ResourceLoader.load_threaded_request(next_scene_path)
 	loading_label.play("loading_text")
 	loading_rods.play("load_opening")
-	loading_rods.play("looped_elements")
 
 func _process(delta):
 	if ResourceLoader.load_threaded_get_status(next_scene_path) == ResourceLoader.THREAD_LOAD_LOADED:
@@ -18,3 +17,7 @@ func _process(delta):
 		var new_scene: PackedScene = ResourceLoader.load_threaded_get(next_scene_path)
 		get_tree().change_scene_to_packed(new_scene)
 	
+
+func _on_loading_rods_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "load_opening":
+		loading_rods.play("looped_elements")
