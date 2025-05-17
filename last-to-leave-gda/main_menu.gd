8fc53628c149dc48a5b28768e72f768d7d964f6b
Baron Remo <159182411+707bremo@users.noreply.github.com>
2025-05-16 22:34:20 -0700

    New controls screen and sound imports
    The controls screen was added into the mix of screens so the user can see what the inputs are for each action.
    
    Gotta know how to play right?

--- last-to-leave-gda/main_menu.gd
+++ last-to-leave-gda/main_menu.gd
@@ -3,27 +3,33 @@ extends Control
 
 # ALL UI ELEMENTS
 @onready var menu_player = $MenuPlayer
+@onready var loopers: AnimationPlayer = $controls_screen/loopers
 @onready var menu_music: AudioStreamPlayer = $SoundsNmoozic/MenuMusic
 @onready var play_button : Button = $play
 @onready var transfer_screen_blocker: Control = $transfer_screen
 @onready var bg: ColorRect = $bg
 @onready var siimlogo: Sprite2D = $siimlogo
 @onready var title: Label = $title
-@onready var lobby_screen: Control = $lobby_screen
 @onready var play_game_button: Button = $lobby_screen/play_game
 @onready var selection_bg: ColorRect = $lobby_screen/selection_bg
+@onready var return_button: Button = $return
 
+# screens
+@onready var lobby_screen: Control = $lobby_screen
+@onready var controls_screen: Control = $controls_screen
 
 # conditions ONLY for screen verification
 var is_start_screen : bool = true
 var is_lobby_screen : bool = true
-
+var is_control_screen : bool = false
 
 
 func _ready() -> void:
 	Input.MOUSE_MODE_VISIBLE
 	is_start_screen = true
 	is_lobby_screen = false
+	is_control_screen = false
+	return_button.visible = false
 	menu_music.play()
 	menu_player.play("main_opening")
 
@@ -59,6 +65,12 @@ func stop_transfer(anim_name: StringName) -> void:
 		selection_bg.visible = false
 		transfer_to_game()
 
+	if anim_name == "selected2":
+		is_control_screen = true
+		is_lobby_screen = false
+		controls_screen.visible = true
+		return_button.visible = true
+		loopers.play("controls_looping")
 
 
 func play_game_pressed():
@@ -66,6 +78,24 @@ func play_game_pressed():
 	selection_bg.visible = true
 	menu_player.play("selected")
 
+func controls_button_pressed() -> void:
+	$SoundsNmoozic/main_select.play()
+	selection_bg.visible = true
+	menu_player.play("selected2")
+
+func exit_button_pressed() -> void:
+	selection_bg.visible = true
+	menu_player.play("selected3")
+	get_tree().quit()
+
+func _on_return_pressed() -> void:
+	if is_control_screen == true:
+		$SoundsNmoozic/main_select.play()
+		controls_screen.visible = false
+		return_button.visible = false
+		is_control_screen = false
+		is_lobby_screen = true
+		menu_player.play("lobby_opening")
 
 func transfer_to_game():
 	LoadingScreenFunctions.load_screen_to_scene("res://world/test_room.tscn")
