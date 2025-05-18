4bbf2fbe179bba0f5d4806cd376485b7710ce71c
blue9200 <Josantoyo@s.sfusd.edu>
2025-05-17 20:00:05 -0700

    MP5/M4A1 Animations

--- last-to-leave-gda/weapon_manager.gd
+++ last-to-leave-gda/weapon_manager.gd
@@ -1,1 +1,90 @@
-extends Node
+extends Node3D
+
+signal Weapon_Changed
+signal Update_Ammo
+signal Update_Weapon_Stack
+
+@onready var Animation_Player = get_node("%AnimationPlayer")
+
+var Current_Weapon = null
+
+var Weapon_Stack = [] #An Array of Weapons Currently Held by the PLayer
+
+var Weapon_Indicator = 0
+
+var Next_Weapon: String
+
+var Weapon_List = {}
+
+@export var _weapon_resource: Array[weapon_resource]
+
+@export var Start_Weapons: Array[String]
+
+func _ready():
+	Initialize(Start_Weapons)
+
+func _input(event):
+	if event.is_action_pressed("Weapon_Up"):
+		Weapon_Indicator = min(Weapon_Indicator+1, Weapon_Stack.size()-1)
+		exit(Weapon_Stack[Weapon_Indicator])
+	
+	if event.is_action_pressed("Weapon_Down"):
+		Weapon_Indicator = max(Weapon_Indicator-1,0)
+		exit(Weapon_Stack[Weapon_Indicator])
+	
+	if event.is_action_pressed("Shoot"):
+		shoot()
+	
+	if event.is_action_pressed("Reload"):
+		reload()
+	
+func Initialize(_start_weapons: Array):
+	for weapon in _weapon_resource:
+		Weapon_List[weapon.Weapon_Name] = weapon
+
+	for i in _start_weapons:
+		Weapon_Stack.push_back(i)
+
+	Current_Weapon = Weapon_List[Weapon_Stack[0]]
+	emit_signal("Update_Weapon_Stack", Weapon_Stack)
+	enter()
+	
+func enter():
+	Animation_Player.queue(Current_Weapon.Activate_Anim)
+	emit_signal("Weapon_Changed", Current_Weapon.Weapon_Name)
+	emit_signal("Update_Ammo", [Current_Weapon.Current_Ammo, Current_Weapon.Reserve_Ammo])
+	
+func exit(_next_weapon: String):
+	if _next_weapon != Current_Weapon.Weapon_Name:
+		if Animation_Player.get_current_animation() != Current_Weapon.Deactivate_Anim:
+			Animation_Player.play(Current_Weapon.Deactivate_Anim)
+			Next_Weapon = _next_weapon
+
+func Change_Weapon(weapon_name: String):
+	Current_Weapon = Weapon_List[weapon_name]
+	Next_Weapon = ""
+	enter()
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == Current_Weapon.Deactivate_Anim:
+		Change_Weapon(Next_Weapon)
+	
+	if anim_name == Current_Weapon.Shoot_Anim && Current_Weapon.Auto_Fire == true:
+		if Input.is_action_pressed("Shoot"):
+			shoot()
+
+func shoot():
+	if Current_Weapon.Current_Ammo !=0:
+		if !Animation_Player.is_playing():
+			Animation_Player.play(Current_Weapon.Shoot_Anim)
+			Current_Weapon.Current_Ammo -= 1 
+			emit_signal("Update_Ammo",[Current_Weapon.Current_Ammo, Current_Weapon.Reserve_Ammo])
+	else:
+		reload()
+
+func reload():
+	if Current_Weapon.Current_Ammo == Current_Weapon.Magazine:
+		return
+	elif !Animation_Player.is_playing():
+		Animation_Player.play(Current_Weapon.Reload_Anim)
+	
--- last-to-leave-gda/weapon_manager/weapon_resource.gd
+++ last-to-leave-gda/weapon_manager/weapon_resource.gd
@@ -1,6 +1,17 @@
-class_name WeaponResource
 extends Resource
 
-@export var view_model : PackedScene
+class_name weapon_resource
 
-@export var damage = 10
+@export var Weapon_Name: String
+@export var Activate_Anim: String
+@export var Shoot_Anim: String
+@export var Reload_Anim: String
+@export var Deactivate_Anim: String
+@export var Out_Of_Ammo_Anim: String
+
+@export var Current_Ammo: int
+@export var Reserve_Ammo: int
+@export var Magazine: int
+@export var Max_Ammo: int
+
+@export var Auto_Fire: bool
